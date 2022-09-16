915bdb1df116f98b9df0e4f6fac386e3d906bbd1
DonnHo <brandonhojj@gmail.com>
2022-09-16 16:10:54 +0800

    Add formula for alpha value of background

--- assignment_1_splashscreen/Src/splashscreen.c
+++ assignment_1_splashscreen/Src/splashscreen.c
@@ -1,7 +1,8 @@
 #include "cprocessing.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 CP_Image logo;
-
+int alpha = 0;
 void splash_screen_init(void)
 {
 	logo = CP_Image_Load("Assets/DigiPen_BLACK.png");
@@ -13,9 +14,15 @@ void splash_screen_init(void)
 
 void splash_screen_update(void)
 {
+	alpha += 3;
+	alpha %= 256;
+	//char buffer[50] = { 0 };
+	//sprintf_s(buffer, _countof(buffer), "Alpha value: %i", alpha);
+	//CP_Font_DrawText(buffer, 30, 30);
+
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), 255);
-	CP_Graphics_DrawCircle(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 2);
+	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), alpha);
+	CP_Graphics_DrawCircle(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 20.0f);
 	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
