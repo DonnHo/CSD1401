659192d71f5f2340c725cdcbac93e08c2c09c564
DonnHo <brandonhojj@gmail.com>
2022-09-16 14:09:07 +0800

    Add circle to position of mouse

--- assignment_1_splashscreen/Src/splashscreen.c
+++ assignment_1_splashscreen/Src/splashscreen.c
@@ -7,7 +7,7 @@ void splash_screen_init(void)
 	logo = CP_Image_Load("Assets/DigiPen_BLACK.png");
 	CP_Settings_ImageMode(CP_POSITION_CORNER);
 	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
-
+	
 	CP_System_SetWindowSize(CP_Image_GetWidth(logo), CP_Image_GetHeight(logo));
 }
 
@@ -15,7 +15,7 @@ void splash_screen_update(void)
 {
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
 	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), 255);
-	CP_Graphics_DrawCircle(1,1,2);
+	CP_Graphics_DrawCircle(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 2);
 	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
