966e5edb15f53abbc147a969bae8a8aa0dd3f15a
DonnHo <brandonhojj@gmail.com>
2022-09-18 18:03:59 +0800

    Add fade out and colour to circle

--- assignment_1_splashscreen/Src/splashscreen.c
+++ assignment_1_splashscreen/Src/splashscreen.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 CP_Image logo;
-int alpha = 0;
+int alphav = 0;
+int increase = 1;
+
 void splash_screen_init(void)
 {
 	logo = CP_Image_Load("Assets/DigiPen_BLACK.png");
@@ -14,15 +16,32 @@ void splash_screen_init(void)
 
 void splash_screen_update(void)
 {
-	alpha += 3;
-	alpha %= 256;
-	//char buffer[50] = { 0 };
-	//sprintf_s(buffer, _countof(buffer), "Alpha value: %i", alpha);
-	//CP_Font_DrawText(buffer, 30, 30);
+	float elapsedtime = CP_System_GetDt();
 
-	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), alpha);
+	if (increase == 1)
+	{
+		alphav += 255 * elapsedtime/4;
+	}
+	if (increase == 0)
+	{
+		alphav -= 255 * elapsedtime/4;
+	}
+	//alphav = alphav % 256; not necessary if doing fade out, causes alpha value to jump to 0 sometimes
+	if (alphav >= 255)
+	{
+		increase = 0;
+	}
+	if (alphav <= 0)
+	{
+		increase = 1;
+	}
+
+	CP_Graphics_ClearBackground(CP_Color_Create(128, 128, 128, 255));
+	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), alphav);
 	CP_Graphics_DrawCircle(CP_Input_GetMouseX(), CP_Input_GetMouseY(), 20.0f);
+
+	CP_Settings_Fill(CP_Color_Create(0, 200, 200, 255));
+
 	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
