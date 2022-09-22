9d288de618eba7cb8b8fde29c03ad8f94da17884
DonnHo <brandonhojj@gmail.com>
2022-09-22 18:23:33 +0800

    Edit rectangle settings to draw from centre

--- assignment_2_csg/Src/mainmenu.c
+++ assignment_2_csg/Src/mainmenu.c
@@ -1,19 +1,35 @@
 #include "cprocessing.h"
-
+#include "carlevel.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void Main_Menu_Init()
 {
-	CP_System_SetWindowSize(1200, 900);
+	CP_System_SetWindowSize(1000, 1000);
 }
 
 void Main_Menu_Update()
 {
 	int buttonW = 200, buttonH = 100;
+
+	int mousex = CP_Input_GetMouseX();
+	int mousey = CP_Input_GetMouseY();
+
+	//mouse x,y
+	CP_Settings_TextSize(20.0f);
+	char buffer1[50] = { 0 };
+	sprintf_s(buffer1, _countof(buffer1), "Mouse x: %i", mousex);
+	CP_Font_DrawText(buffer1, 100, 100);
+	char buffer2[50] = { 0 };
+	sprintf_s(buffer2, _countof(buffer2), "Mouse y: %i", mousey);
+	CP_Font_DrawText(buffer2, 100, 120);
+
 	double windowX = CP_System_GetWindowWidth() / 2;
 	double windowY = CP_System_GetWindowHeight() / 2;
 
 	CP_Graphics_ClearBackground(CP_Color_Create(128, 128, 128, 255));
 	
+	//color
 	CP_Color black = CP_Color_Create(0,0,0,255);
 	CP_Color white = CP_Color_Create(255, 255, 255, 255);
 	CP_Color lblue = CP_Color_CreateHex(0x80dfff);
@@ -21,7 +37,10 @@ void Main_Menu_Update()
 	CP_Color yellow = CP_Color_CreateHex(0x80a0ff);
 	CP_Color red = CP_Color_Create(200, 0 , 0, 255);
 
+	//Text Alignment
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_TOP);
+	//Draw rectangle mode
+	CP_Settings_RectMode(CP_POSITION_CENTER);
 
 	//Centre of window
 	CP_Settings_Stroke(CP_Color_Create(70, 93, 130, 255)); 
@@ -30,18 +49,29 @@ void Main_Menu_Update()
 	CP_Graphics_DrawLine(windowX, 0, windowX, (float)CP_System_GetDisplayHeight()); 
 	//Centre of window
 
+	//start button
 	CP_Settings_Fill(lblue); 
-	CP_Graphics_DrawRect(windowX - 100, windowY - 150, buttonW, buttonH); //button start
-	CP_Settings_Fill(black); 
-	CP_Settings_TextSize(70); //text size
-	CP_Font_DrawText("Start", windowX, windowY - 150); //button start text
-	CP_Settings_Fill(red);
-	CP_Graphics_DrawRect(windowX - 100, windowY + 50, buttonW, buttonH); //button exit
+	CP_Graphics_DrawRect(windowX, windowY - 100, buttonW, buttonH); 
 	CP_Settings_Fill(black);
-	CP_Font_DrawText("Exit", windowX , windowY + 50 ); //button exit text
+	CP_Settings_TextSize(70);
+	CP_Font_DrawText("Start", windowX, windowY - 150);
+
+	//button exit
+	CP_Settings_Fill(red);
+	CP_Graphics_DrawRect(windowX, windowY + 100, buttonW, buttonH);
+	CP_Settings_Fill(black);
+	CP_Font_DrawText("Exit", windowX , windowY + 50 );
 
 
-	
+	if (CP_Input_KeyDown(KEY_Q))
+	{
+		CP_Engine_Terminate();
+	}
+	if (CP_Input_MouseClicked())
+	{
+		
+		CP_Engine_SetNextGameState(Car_Level_Init, Car_Level_Update, Car_Level_Exit);
+	}
 
 }
 
