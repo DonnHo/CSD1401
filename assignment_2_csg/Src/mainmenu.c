52b3585611ab0c2af9db667b9ced363531199fd5
DonnHo <brandonhojj@gmail.com>
2022-09-22 21:18:30 +0800

    Add collision for start and end buttons

--- assignment_2_csg/Src/mainmenu.c
+++ assignment_2_csg/Src/mainmenu.c
@@ -11,21 +11,30 @@ void Main_Menu_Init()
 void Main_Menu_Update()
 {
 	int buttonW = 200, buttonH = 100;
-
-	int mousex = CP_Input_GetMouseX();
-	int mousey = CP_Input_GetMouseY();
-
-	//mouse x,y
-	CP_Settings_TextSize(20.0f);
-	char buffer1[50] = { 0 };
-	sprintf_s(buffer1, _countof(buffer1), "Mouse x: %i", mousex);
-	CP_Font_DrawText(buffer1, 100, 100);
-	char buffer2[50] = { 0 };
-	sprintf_s(buffer2, _countof(buffer2), "Mouse y: %i", mousey);
-	CP_Font_DrawText(buffer2, 100, 120);
+	int mousex = CP_Input_GetMouseX(), mousey = CP_Input_GetMouseY();
 
 	double windowX = CP_System_GetWindowWidth() / 2;
 	double windowY = CP_System_GetWindowHeight() / 2;
+
+	typedef struct buttoncollision
+	{
+		int x1;
+		int x2;
+		int y1;
+		int y2;
+	}button;
+
+	button startbutton, exitbutton;
+
+	startbutton.x1 = windowX - buttonW/2;
+	startbutton.y1 = windowY - 50;
+	startbutton.x2 = windowX + buttonW/2;
+	startbutton.y2 = windowY - 50 - buttonH;
+
+	exitbutton.x1 = windowX - buttonW / 2;
+	exitbutton.y1 = windowY + 50 + buttonH;
+	exitbutton.x2 = windowX + buttonW / 2;
+	exitbutton.y2 = windowY + 50;
 
 	CP_Graphics_ClearBackground(CP_Color_Create(128, 128, 128, 255));
 	
@@ -34,20 +43,13 @@ void Main_Menu_Update()
 	CP_Color white = CP_Color_Create(255, 255, 255, 255);
 	CP_Color lblue = CP_Color_CreateHex(0x80dfff);
 	CP_Color green = CP_Color_CreateHex(0x00F55D);
-	CP_Color yellow = CP_Color_CreateHex(0x80a0ff);
+	CP_Color dred = CP_Color_Create(139, 0, 0, 255);
 	CP_Color red = CP_Color_Create(200, 0 , 0, 255);
 
 	//Text Alignment
 	CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_TOP);
 	//Draw rectangle mode
 	CP_Settings_RectMode(CP_POSITION_CENTER);
-
-	//Centre of window
-	CP_Settings_Stroke(CP_Color_Create(70, 93, 130, 255)); 
-	CP_Settings_StrokeWeight(1.0f);
-	CP_Graphics_DrawLine(0, windowY, (float)CP_System_GetDisplayWidth(), windowY);
-	CP_Graphics_DrawLine(windowX, 0, windowX, (float)CP_System_GetDisplayHeight()); 
-	//Centre of window
 
 	//start button
 	CP_Settings_Fill(lblue); 
@@ -67,12 +69,51 @@ void Main_Menu_Update()
 	{
 		CP_Engine_Terminate();
 	}
-	if (CP_Input_MouseClicked())
+	
+	//Start button collision
+	if (mousex > startbutton.x1 && mousex < startbutton.x2)
 	{
-		
-		CP_Engine_SetNextGameState(Car_Level_Init, Car_Level_Update, Car_Level_Exit);
+		if (mousey < startbutton.y1 && mousey > startbutton.y2)
+		{
+			CP_Settings_Fill(green);
+			CP_Graphics_DrawRect(windowX, windowY - 100, buttonW, buttonH);
+			if (CP_Input_MouseClicked())
+			{
+
+				CP_Engine_SetNextGameState(Car_Level_Init, Car_Level_Update, Car_Level_Exit);
+			}
+		}
 	}
 
+	//Exit button collision
+	if (mousex > exitbutton.x1 && mousex < exitbutton.x2)
+	{
+		if (mousey < exitbutton.y1 && mousey > exitbutton.y2)
+		{
+			CP_Settings_Fill(dred);
+			CP_Graphics_DrawRect(windowX, windowY + 100, buttonW, buttonH);
+			if (CP_Input_MouseClicked())
+			{
+
+				CP_Engine_Terminate();
+			}
+		}
+	}
+
+	//Centre of window
+	CP_Settings_Stroke(CP_Color_Create(70, 93, 130, 255));
+	CP_Settings_StrokeWeight(1.0f);
+	CP_Graphics_DrawLine(0, windowY, (float)CP_System_GetDisplayWidth(), windowY);
+	CP_Graphics_DrawLine(windowX, 0, windowX, (float)CP_System_GetDisplayHeight());
+
+	//mouse x,y
+	CP_Settings_TextSize(20.0f);
+	char buffer1[50] = { 0 };
+	sprintf_s(buffer1, _countof(buffer1), "Mouse x: %i", mousex);
+	CP_Font_DrawText(buffer1, 100, 100);
+	char buffer2[50] = { 0 };
+	sprintf_s(buffer2, _countof(buffer2), "Mouse y: %i", mousey);
+	CP_Font_DrawText(buffer2, 100, 120);
 }
 
 void Main_Menu_Exit()
